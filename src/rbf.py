a4ebfdefbf84118adbaa27b134c66b9b037a1a7e
Yash Jakhotiya <mailsforyashj@gmail.com>
2019-11-19 14:55:06 +0530

    Add rbf kernel

---
+++ src/rbf.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.layers import Layer
+from tensorflow.keras import backend as K
+
+class RBFLayer(Layer):
+    def __init__(self, units, gamma, **kwargs):
+        super(RBFLayer, self).__init__(**kwargs)
+        self.units = units
+        self.gamma = K.cast_to_floatx(gamma)
+
+    def build(self, input_shape):
+        self.mu = self.add_weight(name='mu',
+                                  shape=(int(input_shape[1]), self.units),
+                                  initializer='uniform',
+                                  trainable=True)
+        super(RBFLayer, self).build(input_shape)
+
+    def call(self, inputs):
+        diff = K.expand_dims(inputs) - self.mu
+        l2 = K.sum(K.pow(diff,2), axis=1)
+        res = K.exp(-1 * self.gamma * l2)
+        return res
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.units)
